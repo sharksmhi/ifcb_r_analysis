cdec41532a522b5a3d2f868a58c5db62281ab3f1
Torstensson Anders <a002440@ad.smhi.se>
2023-11-09 21:25:18 +0100

    Initial commit

---
+++ code/ifcb_import_annotated_images.R
@@ -0,0 +1,39 @@
+library(R.matlab)
+
+# Define your ifcb data path
+ifcb_path <- ""
+
+classifier <- "Baltic" #
+
+taxa <- "Cryptomonadales"
+
+year <- 2023
+
+imagedir <- paste(ifcb_path, "work/data/classified_images", classifier, taxa, sep = "/")
+manualdir <- paste(ifcb_path, "work/data/manual", classifier, sep = "/")
+
+imagefiles <- list.files(imagedir, pattern="png$", full.names = FALSE, recursive = TRUE)
+manualfiles <- list.files(manualdir, pattern="mat$", full.names = FALSE, recursive = TRUE)
+
+sample <- sub("_.*", "", imagefiles)
+roi <- as.numeric(gsub(".*[_]([^.]+)[.].*", "\\1", imagefiles))
+
+annotated.df <- data.frame(roi, taxa, sample)
+
+manual.mat <- readMat(file.path(manualdir, paste0(sample[1], "_IFCB134.mat")))
+
+annotated_matrix <- annotated.df %>% 
+  filter(sample == sample[1]) %>%
+  mutate(taxa = 19) %>%
+  select(-sample) %>%
+  as.matrix()
+
+for(i in 1:nrow(manual.mat$classlist)) {
+  if(manual.mat$classlist[i,1] %in% annotated_matrix[,1]) {
+    if(manual.mat$classlist[i,2] == 1) {
+      manual.mat$classlist[i,2] <- unique(annotated_matrix[,2])
+    }
+  }
+}
+
+write.csv(manual.mat$classlist, file.path(manualdir, "additions", paste0(sample[1], "_IFCB134.csv")), row.names = FALSE)
