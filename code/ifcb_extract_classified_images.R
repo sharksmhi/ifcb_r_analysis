a57cac593d811b89cf1dbfa028446e8daafbba2d
Torstensson Anders <a002440@ad.smhi.se>
2023-11-16 12:30:06 +0100

    Added possibility to only use classes in classifier

--- code/ifcb_extract_classified_images.R
+++ code/ifcb_extract_classified_images.R
@@ -5,13 +5,16 @@ source("code/fun/get_class2use.R")
 
 # Define your ifcb data path
 ifcb_path <- ""
-config_path <- ""
+config_path <- paste0(ifcb_path, "/config")
 out_path <- "output/"
+classifier_path_b <- paste0(ifcb_path, "/classified/Baltic/2023")
+classifier_path_sk <- paste0(ifcb_path, "/classified/Skagerrak-Kattegat/2023")
 
 # Define filenames
 sample_classifier_file <- "sample_classifier.txt"
 class2use_B_file <- "class2use_Baltic.mat"
 class2use_SK_file <- "class2use_Kattegat-Skagerrak.mat"
+classified_file <- "D20230311T092911_IFCB134_class_v1.mat"
 
 # Choose taxa to extract
 taxa_baltic <- data.frame(
@@ -40,13 +43,15 @@ classes_baltic <- data.frame(
   taxa = get_class2use(file.path(config_path, class2use_B_file)),
   classifier = "Baltic") %>%
   filter(!taxa == "unclassified") %>%
-  filter(!taxa %in% taxa_baltic$taxa)
+  filter(!taxa %in% taxa_baltic$taxa) %>%
+  filter(taxa %in% get_class2use(file.path(classifier_path_b, classified_file)))
 
 # Create df with SK class2use, exluding unclassified
 classes_skagerrak_kattegat <- data.frame(
   taxa = get_class2use(file.path(config_path, class2use_SK_file)),
   classifier = "Skagerrak-Kattegat") %>%
-  filter(!taxa == "unclassified")
+  filter(!taxa == "unclassified") %>%
+  filter(taxa %in% get_class2use(file.path(classifier_path_sk, classified_file)))
 
 # Bind tables
 taxa_all <- rbind(classes_baltic, classes_skagerrak_kattegat)
@@ -97,7 +102,9 @@ taxa_all <- taxa_all %>%
   filter(!paste(taxa, classifier) == "Dactyliosolen fragilissimus Baltic") %>%
   filter(!paste(taxa, classifier) == "Peridiniella catenata Baltic") %>%
   filter(!paste(taxa, classifier) == "Prorocentrum cordatum Baltic") %>%
-  filter(!paste(taxa, classifier) == "Mesodinium rubrum Baltic")
+  filter(!paste(taxa, classifier) == "Mesodinium rubrum Baltic") %>%
+  filter(!paste(taxa, classifier) == "Skeletonema marinoi Baltic") %>%
+  filter(!paste(taxa, classifier) == "Heterocapsa rotundata Baltic")
 
 for(i in 1:length(taxa_all$taxa)) {
   
@@ -110,5 +117,7 @@ for(i in 1:length(taxa_all$taxa)) {
     cat("Extracting ", taxa, " images from sample ", j, "/", length(samples$sample), "\n", sep = "")
     
     extract_taxa_images(samples$sample[j], samples$classifier[j], ifcb_path, taxa) 
+    gc()
   }
+  
 }
