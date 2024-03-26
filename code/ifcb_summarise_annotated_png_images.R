6b32bd4c12cbb3b2daaac34638fa2a4867bf65a7
anderstorstensson <anders.torstensson@smhi.se>
2024-03-26 12:58:18 +0100

    add summary of n annotated images per sample

--- code/ifcb_summarise_annotated_png_images.R
+++ code/ifcb_summarise_annotated_png_images.R
@@ -1,17 +1,28 @@
 library(tidyverse)
 
-pngdir <- "//winfs-proj/data/proj/ifcb/work/data/png_images/"
+extraction_date <- "2024-03-26"
+classifier <- "Skagerrak_Kattegat"
+
+pngdir <- file.path("//winfs-utv/data/utv/ifcb/work/data/png_images", classifier, extraction_date)
 plotdir <- paste0(here::here(),"/plots/classifier/")
 outputdir <- paste0(here::here(),"/output/classifier/")
 
 files <- list.files(pngdir, pattern="png$", full.names = TRUE, recursive = TRUE)
 
-files_df <- data_frame(
+files_df <- tibble(
   dir = dirname(files)) %>% 
   count(dir) %>% 
   mutate(taxa = basename(dir),
-         classifier = basename(dirname(dir))) %>%
+         classifier = sub(".*/png_images/(.*?)/.*", "\\1", dir)) %>%
   arrange(desc(n))
+
+sample_summary <- tibble(file = files,
+                   filename = basename(files)) %>%
+  mutate(sample_name = gsub("_IFCB.*", "", filename)) %>%
+  count(sample_name, name = "n_images") %>%
+  arrange(sample_name) %>%
+  mutate(date = as.Date(substr(sample_name, 2, 9), format = "%Y%m%d")) %>%
+  relocate(date)
 
 for(i in 1:length(unique(files_df$classifier))) {
   files_df_ix <- files_df %>%
@@ -41,6 +52,14 @@ for(i in 1:length(unique(files_df$classifier))) {
 # Save data as a txt file
 write.table(files_df, 
             paste0(outputdir, "n_images.txt"),
+            sep = "\t",
+            quote = FALSE, 
+            na = "NA", 
+            row.names=F)
+
+# Save data as a txt file
+write.table(sample_summary, 
+            paste0(outputdir, classifier, "_", "n_images_per_sample", "_", extraction_date, ".txt"),
             sep = "\t",
             quote = FALSE, 
             na = "NA", 
