b08e21679cfc12086936a034aeafe91d48fc5431
anderstorstensson <anders.torstensson@smhi.se>
2024-06-02 15:11:00 +0200

    add additional code to modify classlist

---
+++ code/R/ifcb_replace_nan_in_classlist.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(reticulate)
+
+# Setup virtual environment
+virtualenv_create("code/python/venv", requirements = "code/python/requirements.txt")
+use_virtualenv("code/python/venv")
+
+# Now try to import the python function
+source_python("code/python/replace_nan_in_classlist.py")
+
+# Define which classifier you are working on
+classifier <- "Tångesund"
+
+# List files to be updated
+files <- list.files(file.path("output/manual", classifier))
+
+for (i in 1:length(files)) {
+  replace_nan_in_classlist(file.path("output/manual", classifier, files[i]),  # Ensure correct file path
+                           file.path("output/manual", classifier, files[i]),
+                           19)  # Ensure correct output file path)
+}
